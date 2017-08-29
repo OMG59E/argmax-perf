ae597ff47307f41050a4c3990bba7572dfbad222
Pin Gao <pin.gao2008@gmail.com>
2017-08-29 20:52:34 +0000

    Hand written kernel takes around 50us.

--- cuda/argmax.cu
+++ cuda/argmax.cu
@@ -3,6 +3,8 @@
 #include <cuda_runtime.h>
 #include <limits>
 #include <glog/logging.h>
+#include <stdio.h>
+#include "./util.h"
 
 
 #define CUCHECK(stat) { cudaErrCheck_((stat), __FILE__, __LINE__); }
@@ -24,14 +26,12 @@ using TI = int;
  * Each thread do reduction, and write to global shared memory.
  */
 
-__device__ thread_reduce(TD* data, TD* data_out, TI* index_out, int bsize, int hsize) { 
+__device__ void thread_reduce(TD* data, TD* shared_data, TI* shared_index, TD lowest, int bsize, int hsize) { 
   //if (threadIdx.x > hsize) return;
   if (blockIdx.x > bsize) return;
 
   TD * batch_data = data + blockIdx.x * hsize;
-  //TD  thread_local_max_data = batch_data[threadIdx.x];
-  //TI  thread_local_max_idx = threadIdx.x;
-  TD  thread_local_max_data = std::numeric_limits<TD>::lowest();
+  TD  thread_local_max_data = lowest;
   TI  thread_local_max_idx = 0;
 
   for (int i = threadIdx.x ; i < hsize; i += blockDim.x) {
@@ -40,15 +40,17 @@ __device__ thread_reduce(TD* data, TD* data_out, TI* index_out, int bsize, int h
       thread_local_max_idx = i;
     }
   }
-  data_out[blockIdx.x][threadIdx.x] = thread_local_max_data;
-  index_out[blockIdx.x][threadIdx.x] = thread_local_max_idx;
+  
+  shared_data[threadIdx.x] = thread_local_max_data;
+  shared_index[threadIdx.x] = thread_local_max_idx;
 }
 
-__device__ block_reduce(TD* shared_data, TI* shared_index) {
-  TD left, right;
+
+__device__ void block_reduce(TD* shared_data, TI* shared_index) {
+  int left, right;
   int threads = blockDim.x / 2;
 
-  for (stride = 1; stride < blockDim.x; stride *= 2, threads /=2 ) {
+  for (int stride = 1; stride < blockDim.x; stride *= 2, threads /=2 ) {
     if (threadIdx.x < threads) {
       left = threadIdx.x * (stride * 2);
       right = left + stride;
@@ -61,29 +63,30 @@ __device__ block_reduce(TD* shared_data, TI* shared_index) {
   }
 }
 
-__global__ argmax_kernel(TD* data, TI* index_out, int bsize, int hsize) { 
+__global__ void argmax_kernel(TD* data, TI* index_out, TD lowest, int bsize, int hsize) { 
   // shared memory  
-  __shared__ TD shared_data[gridDim.x][blockDim.x];
-  __shared__ TI shared_index[gridDim.x][blockDim.x];
+  extern __shared__ int s[]; 
+  TD* shared_data = reinterpret_cast<TD*>(s);
+  TI* shared_index = reinterpret_cast<TI*>(shared_data + blockDim.x);
 
-  thread_reduce(data, shared_data, shared_index, bsize, hsize);
+  thread_reduce(data, shared_data, shared_index, lowest, bsize, hsize);
   __syncthreads();
   // for each block do block reduce
   block_reduce(shared_data, shared_index);
 
   // write to global memory
-  if (threadIdx.x < bsize) { 
-    index_out[threadIdx.x] = shared_index[blockIdx.x][threadIdx.x];
+  if (threadIdx.x == 0) {
+    index_out[blockIdx.x] = shared_index[0];
   }
 }
 
 
 int main(int argc, char **argv) {
-  int bsize = 1;
+  int bsize = 64;
   int hsize = 30000;
 
-  TD* data_host = malloc(sizeof(TD) * hsize * bsize);
-  TI* index_host = malloc(sizeof(TI) * bsize);
+  TD* data_host = (TD*)malloc(sizeof(TD) * hsize * bsize);
+  TI* index_host = (TI*)malloc(sizeof(TI) * bsize);
   
   for (int i = 0;i < bsize;i ++) { 
     for (int j = 0;j < hsize;j ++) { 
@@ -91,30 +94,39 @@ int main(int argc, char **argv) {
     }
   }
 
+  LOG(INFO) << " host set up ok.";
   // copy to cuda
   TD * data_dev;
   TI * index_dev;
-  CUCHECK(cudaMalloc(static_cast<void**>(&data_dev), sizeof(TD) * hsize * bsize));
-  CUCHECK(cudaMalloc(static_cast<void**>(&index_dev), sizeof(TI) * bsize));
+  CUCHECK(cudaMalloc(reinterpret_cast<void**>(&data_dev), sizeof(TD) * hsize * bsize));
+  CUCHECK(cudaMalloc(reinterpret_cast<void**>(&index_dev), sizeof(TI) * bsize));
 
-  CUCHECK(cudaMemcpy(data_dev, data_host, sizeof(TD) * hsize * bsize), cudaMemcpyHostToDevice);
+  CUCHECK(cudaMemcpy(data_dev, data_host, sizeof(TD) * hsize * bsize, cudaMemcpyHostToDevice));
   //CUCHECK(cudaMemcpy(index_dev, index_host, sizeof(TD) * hsize * bsize), cudaMemcpyHostToDevice);
 
+  LOG(INFO) << " dev set up ok.";
   // launch kernel
   int numBlocks = bsize;
   int threadsPerBlock = 512;
+  size_t dev_sm_bytes = threadsPerBlock * (sizeof(TD) + sizeof(TI));
 
-  argmax_kernel<<<numBlocks, threadsPerBlock>>>(data_dev, index_dev);
+  for (int i = 0;i < 10;i ++) {
+    uint64_t t0, t1;
+    t0 = get_time();
+    argmax_kernel<<<numBlocks, threadsPerBlock, dev_sm_bytes>>>(data_dev, index_dev, 
+      std::numeric_limits<TD>::lowest(), bsize, hsize);
+    CUCHECK(cudaDeviceSynchronize());
+    t1 = get_time();
+    LOG(INFO) << " takes us : " << t1 - t0;
+  }
 
-  CUCHECK(cudaDeviceSynchronize());
-
+  LOG(INFO) << " argmax ok.";
   // get output and print
-  CUCHECK(cudaMemcpy(index_host, index_dev, sizeof(TI) * bsize), cudaMemcpyDeviceToHost);
+  CUCHECK(cudaMemcpy(index_host, index_dev, sizeof(TI) * bsize, cudaMemcpyDeviceToHost));
   
   for (int i = 0;i < bsize;i ++) { 
     LOG(INFO) << " " << index_host[i];
   }
-
 
   return 0;
 }
